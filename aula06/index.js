f74ab203fcb63f915ab49a4ed24b47378d0afe61
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2023-12-04 15:06:41 -0300

    revisão javascript

--- aula06/index.js
+++ aula06/index.js
@@ -10,7 +10,7 @@ let soma = alert(`A soma de ${n1} + ${n2} é = ${num1+num2}`)
 
 
 
-let nome = prompt('DIGITE SEU NOME:')
+/* let nome = prompt('DIGITE SEU NOME:')
 let sobre = prompt('DIGITE SEU SOBRENOME:')
 
 let anoatual = prompt('DIGITE O ANO ATUAL')
@@ -21,4 +21,12 @@ let n1 = Number(ano)
 
 let soma = anoatual - ano
 
-let res = document.write(`<p> Seu nome é: <strong>${nome}</strong> <br> Seu sobrenome é: <strong>${sobre}</strong> <br> Você tem <strong>${soma}</strong de idade</p>`)
+let res = document.write(`<p> Seu nome é: <strong>${nome}</strong> <br> Seu sobrenome é: <strong>${sobre}</strong> <br> Você tem <strong>${soma}</strong de idade</p>`) */
+
+let a = 5 + 3
+let b = a % 5
+let c = 5 * b ** 2
+let d = 10 - a / 2
+let e = 6 * 2 / d
+let f = b % e + 4 / 2
+console.log(f)
