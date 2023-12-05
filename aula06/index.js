160b3bc0286b8b50f053e42ccbad1f949c63c0e5
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2023-12-05 14:58:12 -0300

    atualizado javascript

--- aula06/index.js
+++ aula06/index.js
@@ -23,10 +23,21 @@ let soma = anoatual - ano
 
 let res = document.write(`<p> Seu nome é: <strong>${nome}</strong> <br> Seu sobrenome é: <strong>${sobre}</strong> <br> Você tem <strong>${soma}</strong de idade</p>`) */
 
-let a = 5 + 3
+/* let a = 5 + 3
 let b = a % 5
 let c = 5 * b ** 2
 let d = 10 - a / 2
 let e = 6 * 2 / d
-let f = b % e + 4 / 2
-console.log(f)
+let f = b % e + 4 / 2 */
+
+/* let n =10
+let n1 = 20
+let n3 = 20
+ */
+
+let a = 5
+let b = 8
+
+
+
+console.log(true || false )
