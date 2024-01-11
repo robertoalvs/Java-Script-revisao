c80cf5020315b03f265e23901a8531c1b27388a7
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2024-01-11 17:11:52 -0300

    atualizado condições

--- 2024-java-script/index.js
+++ 2024-java-script/index.js
@@ -10,26 +10,33 @@ function verificar() {
     let ano = data.getFullYear()
     let dia = data.getDate()
 
-    msg.innerHTML = `Estamos em ${ano} e são ${hora} horas da manhã`
-
-
+    
     if (hora >= 0 && hora < 12) {
 
-        foto.src = '../teste.if/img/manha.png'
+        msg.innerHTML = `Estamos em ${ano} e são ${hora} horas da manhã`
 
-        res.innerHTML = `Boa dia hoje, hoje é dia <strong>${dia}</strong>`
+        res.innerHTML = `Bom dia, hoje é dia <strong>${dia}</strong>`
+
+        foto.src = '../teste.if/img/manha.png'
+        
 
     } else 
             if (hora >= 12 && hora < 18) {
 
-                foto.src = '../teste.if/img/tarde.png'
+                msg.innerHTML = `Estamos em ${ano} e são ${hora} horas da tarde`
 
-                res.innerHTML = `Boa tarde hoje é dia <strong>${dia}</strong>`
+                res.innerHTML = `Boa tarde, hoje é dia <strong>${dia}</strong>`
+
+                foto.src = '../teste.if/img/tarde.png'
 
             } else {
 
+                msg.innerHTML = `Estamos em ${ano} e são ${hora} horas da noite`
+
+                res.innerHTML = `Boa noite, hoje é dia <strong>${dia}</strong>`
+
+
                 foto.src = '../teste.if/img/noite.png'
 
-                res.innerHTML = `Boa noite hoje é dia <strong>${dia}</strong>`
             }
 }
