7e114c61770ead6fec0c1f8f9783ee701029d68a
Roberto Alves <robertoalvss@gmail.com>
2024-01-17 11:40:34 -0300

    atualizado

--- 2024-java-idade/index.js
+++ 2024-java-idade/index.js
@@ -17,36 +17,22 @@ function clicar() {
     let genero = ''
     
 
-    if (numero1.value == 0 || num > ano) {
+    if (numero1.value == 0 || numero1.value > ano) {
 
-        alert('[ERRO! Você digitou o ano de nascimento errado, ou não digitou nada]')
-
-    } else  
-            if(homem && idade <= 11){
-
-        genero = 'Homem'
-
-        fotos.src ='../2024-java-idade/img/crianca-homem.png'
-        resposta.innerHTML = `Você é: ${genero} e tem ${idade} anos `
-
-    } else 
-            if(homem && idade >= 12 && idade < 17) {
-            
-                genero = 'Homem'
-
-             fotos.src ='../2024-java-idade/img/adolecente-menino.png'
-             resposta.innerHTML = `Você é: ${genero} e tem ${idade} anos `
-                
-    } else {
-
-            if(homem && idade >= 18 && idade <= 59)
-
-             genero = 'Homem'
-
-        fotos.src ='../2024-java-idade/img/homem-adulto.png'
-        resposta.innerHTML = `Você é: ${genero} e tem ${idade} anos `
+        alert('[ERRO!] Você não digitou nada, ou colocou o ano errado, [CORRIGIR]')
 
 
+    } else if (homem && idade == 0 && idade <= 11) {
+
+            genero = 'Homem'
+            fotos.src ='../2024-java-idade/img/crianca-homem.png'
+            resposta.innerHTML = `Você é ${genero} e você tem ${idade}`
+
+    } else  if (homem && idade >= 12 ) {
+
+            genero = 'Homem'
+            fotos.src ='../2024-java-idade/img/adolecente-menino.png'
+            resposta.innerHTML = `Você é ${genero} e você tem ${idade}`
     } 
 
 
