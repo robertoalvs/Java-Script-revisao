e3390ddf399606bdb143c8350ff70dc73b83a6c1
Roberto Alves <robertoalvss@gmail.com>
2024-01-18 14:29:31 -0300

    atualização condições if and else

--- 2024-java-idade/index.js
+++ 2024-java-idade/index.js
@@ -4,6 +4,8 @@ function clicar() {
     let fotos = document.querySelector('img#foto')
     let numero1 = document.querySelector('input#inum')
     let sex = document.getElementsByName('sexo')
+    let imasc = document.querySelector('input#imasc')
+    let ifem = document.querySelector('input#ifem')
     let resposta = document.querySelector('div#res')
 
     let data = new Date()
@@ -22,24 +24,62 @@ function clicar() {
         alert('[ERRO!] Você não digitou nada, ou colocou o ano errado, [CORRIGIR]')
 
 
-    } else if (homem ) {
+    }  else {
+
+        if(homem) {
 
             genero = 'Homem'
-            fotos.src ='../2024-java-idade/img/crianca-homem.png'
-            resposta.innerHTML = `Você é ${genero} e você tem ${idade}`
 
-    } else  if (homem || idade >= 12 || idade <= 17) {
+            if (idade == 0 || idade <= 11) {
 
-            genero = 'Homem'
-            fotos.src ='../2024-java-idade/img/adolecente-menino.png'
-            resposta.innerHTML = `Você é ${genero} e você tem ${idade}`
+                fotos.src ='../2024-java-idade/img/crianca-homem.png'
+                resposta.innerHTML = `Você é uma ${genero} e tem ${idade} anos`
 
-    } else  if(homem || idade >= 18 || idade <= 59) {
+            } else if (idade <= 17 ) {
 
-        genero = 'Homem'
-        fotos.src ='../2024-java-idade/img/homem-adulto.png'
-        resposta.innerHTML = `Você é ${genero} e você tem ${idade}`
+                fotos.src ='../2024-java-idade/img/adolecente-menino.png'
+                resposta.innerHTML = `Você é uma ${genero} e tem ${idade} anos`
+
+            } else if (idade <= 59 ) {
+
+                fotos.src ='../2024-java-idade/img/homem-adulto.png'
+                resposta.innerHTML = `Você é uma ${genero} e tem ${idade} anos`
+
+            } else  {
+
+                fotos.src ='../2024-java-idade/img/homem-idoso.png'
+                resposta.innerHTML = `Você é uma ${genero} e tem ${idade} anos`
+
+            }
+
+        } else if (mulher) {
+
+            genero = 'Mulher'
+
+            if (idade == 0 || idade <= 11) {
+
+                fotos.src ='../2024-java-idade/img/crianca-mulher.png'
+                resposta.innerHTML = `Você é uma ${genero} e tem ${idade} anos`
+
+            } else if (idade <= 17) {
+
+                fotos.src ='../2024-java-idade/img/adolecente-menina.png'
+                resposta.innerHTML = `Você é uma ${genero} e tem ${idade} anos`
+
+            } else if (idade <= 59) {
+
+                fotos.src ='../2024-java-idade/img/mulher-adulta.png'
+                resposta.innerHTML = `Você é uma ${genero} e tem ${idade} anos`
+
+            } else {
+
+                fotos.src ='../2024-java-idade/img/mulher-idosa.png'
+                resposta.innerHTML = `Você é uma ${genero} e tem ${idade} anos`
+
+            }
+        }
+        
     }
-
+         
 
 }
