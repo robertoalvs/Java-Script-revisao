720700d7ffdf899a69f3997475c0c7a069ad005f
Roberto Alves <robertoalvss@gmail.com>
2024-01-30 14:37:01 -0300

    atualizações repetições While do while for

--- Repetições-JS/index.js
+++ Repetições-JS/index.js
@@ -1,2 +1,28 @@
+function contar() {
 
 
+    let n1 = document.querySelector('input#iini')
+    let n2 = document.querySelector('input#ifim')
+    let n3 = document.querySelector('input#ipasso')
+
+    let num1 = Number(n1.value)
+    let num2 = Number(n2.value)
+    let num3 = Number(n3.value)
+
+    let res = document.querySelector('div#res')
+
+
+    if (num1 == 0 || num2 == 0 || num3 == 0) {
+
+        alert('ERRO! CORRIGIR')
+
+
+    } else {
+
+        for (let c = num1; c <= num2; c += num3) {
+
+            res.innerHTML += `${c}`
+        }
+
+    } 
+ }
